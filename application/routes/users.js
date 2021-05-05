3efd5365ac496c2569d7524525df765aef78edb5
Jade Simien <jsimien@mail.sfsu.edu>
2021-05-05 08:46:32 -0700

    Fixed login

--- application/routes/users.js
+++ application/routes/users.js
@@ -19,9 +19,60 @@ router.post('/registration', (req, res, next) => {
   let username = req.body.username;
   let email = req.body.email;
   let password = req.body.password;
-  /**
-   * js form validation implemented
-   */
+  
+  // do server side validation
+const username = document.getElementById('username')
+const password = document.getElementById('password')
+const redo = document.getElementById('redo')
+const register = document.getElementById('register')
+const errorElement = document.getElementById('error')
+const ages = document.querySelectorAll('input[name="age"]');
+let value;
+var req = /(?=.*\d)(?=.*[A-Z])(?=.*[a-z])(?=.*\W)/;
+var check = /^[a-zA-Z]/;
+
+register.addEventListener('submit', (e) => {
+    let messages = []
+    if (username.value == '' || username.value == null || username.value.length < 3) {
+        messages.push('\nUsername must be 3+ alphanumeric characters long')
+    }
+    if(!check.test(username.value)) {
+        messages.push('\nUsername must begin with a letter')
+    }
+
+    if (password.value.length < 8) {
+        messages.push('\nPassword must be 8+ characters long')
+    }
+
+    if (!req.test(password.value)) {
+        messages.push('\nPassword must have at least 2 letters of different cases, 1 number, and 1 special character')
+    }
+
+    if(password.value != redo.value) {
+        messages.push('\nPasswords must match')
+    }
+
+    for (const age of ages) {
+        if(age.checked) {
+            value = age.value;
+            break;
+        }
+    }
+    var ageno = "no";
+    if(value == ageno) {
+        messages.push('\nYou must be 13 or older');
+    }
+
+    if (messages.length > 0) {
+        e.preventDefault()
+        errorElement.innerText = messages.join(', ')
+        alert(messages.join(', '))
+    }
+})
+
+// end validation
+
+
 
   db.execute("SELECT * FROM users WHERE username=?", [username])
   .then(([results, fields]) => {
@@ -80,10 +131,26 @@ router.post('/login', (req, res, next) => {
   let username = req.body.username;
   let password = req.body.password;
 
-  // do server side validation
+  // validation
+  const username = document.getElementById('username');
+  const password = document.getElementById('password');
 
+  register.addEventListener('submit', (e) => {
 
+    if (username.value == '' || username.value == null || username.value.length < 3) {
+      messages.push('\nUsername must be 3+ alphanumeric characters long')
+  }
+    
+    if (password.value.length < 8) {
+    messages.push('\nPassword must be 8+ characters long')
+    }
 
+    if (messages.length > 0) {
+      e.preventDefault()
+      errorElement.innerText = messages.join(', ')
+      alert(messages.join(', '))
+  }
+  })
 
 
 
